5a27bcdbfe23957287e9d19d7db50df0f5190dbe
Leo Abe <47789494+leonardoabe@users.noreply.github.com>
2024-11-06 18:27:47 -0300

    Implementing hibernate into person entity

--- src/main/java/domain/Person.java
+++ src/main/java/domain/Person.java
@@ -1,29 +1,33 @@
 package domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serial;
 import java.io.Serializable;
 
+@Entity
 public class Person implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String name;
+
     private String email;
 
-    public Person(Integer id, String name, String email) {
-        this.id = id;
+    public Person(String name, String email) {
         this.name = name;
         this.email = email;
     }
 
     public Integer getId() {
         return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
     }
 
     public String getName() {
